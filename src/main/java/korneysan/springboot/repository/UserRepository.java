2380174b03dfaae6e496120e69c11b32129e3bf8
Alexander Kornienko <korneysan@tut.by>
2022-10-11 20:29:46 +0300

    3.1.1 Repository annotation

--- src/main/java/korneysan/springboot/repository/UserRepository.java
+++ src/main/java/korneysan/springboot/repository/UserRepository.java
@@ -2,6 +2,8 @@ package korneysan.springboot.repository;
 
 import korneysan.springboot.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 }
